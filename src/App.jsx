5394600acb1348b4a173af1efd96490f2505ee74
parthiban <rparthiban987@gmail.com>
2026-02-16 15:03:32 +0530

    App.jsx and  app.css file are added

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home_page from './Home_page'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home_page/>}></Route>
+    </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
